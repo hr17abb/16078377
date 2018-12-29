c335d2aa7c929abfce789e186d00e9b23488bf75
hr17abb <45021130+hr17abb@users.noreply.github.com>
2018-12-29 16:33:27 +0000

    Birds and coins coming from the right direction
    the coins are not hiding after collision, colliding like the birds

--- airplane/airplane/ViewController.swift
+++ airplane/airplane/ViewController.swift
@@ -29,11 +29,10 @@ class ViewController: UIViewController, subviewDelegate {
     
     var allbirds: [UIImageView] = []
     
-    //var starts = 20
-    //var startsTimer = 0
+    var starts = 20
+    var startsTimer = Timer()
     var points = 0
-    var starts = 0;
-    var Timer = 20;
+    
     
     @objc func playButton (sender: UIButton){
         gameover.removeFromSuperview()
@@ -61,8 +60,24 @@ class ViewController: UIViewController, subviewDelegate {
     @IBOutlet weak var bird8: UIImageView!
     @IBOutlet weak var bird9: UIImageView!
     @IBOutlet weak var bird10: UIImageView!
+    @IBOutlet weak var bird11: UIImageView!
+    @IBOutlet weak var bird12: UIImageView!
+    @IBOutlet weak var bird13: UIImageView!
+    @IBOutlet weak var bird14: UIImageView!
+    @IBOutlet weak var bird15: UIImageView!
+    @IBOutlet weak var bird16: UIImageView!
+    @IBOutlet weak var bird17: UIImageView!
+    @IBOutlet weak var bird18: UIImageView!
+    @IBOutlet weak var bird19: UIImageView!
+    @IBOutlet weak var bird20: UIImageView!
+    
     
     @IBOutlet weak var coin: UIImageView!
+    @IBOutlet weak var coin1: UIImageView!
+    @IBOutlet weak var coin2: UIImageView!
+    @IBOutlet weak var coin3: UIImageView!
+    @IBOutlet weak var coin4: UIImageView!
+    @IBOutlet weak var coin5: UIImageView!
     
     @IBOutlet weak var roadimage: UIImageView!
     
@@ -74,9 +89,7 @@ class ViewController: UIViewController, subviewDelegate {
     
     @IBOutlet weak var replay: UIButton!
     
-    
-    
-    /*let birdsArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    let birdsArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
     
     @objc func startGameTimer() {
         starts = starts - 1
@@ -85,14 +98,16 @@ class ViewController: UIViewController, subviewDelegate {
         if starts == 0{
             startsTimer.invalidate()
         }
-    
+        
         startsTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector (ViewController.startGameTimer),userInfo: nil, repeats: true)
         starts = 20
         GameTimer.setTitle(String(starts), for: .normal)
         
         replay.isHidden = true
-    
-}*/
+        
+        
+        
+    }
     
     
     override func viewDidLoad() {
@@ -143,117 +158,44 @@ class ViewController: UIViewController, subviewDelegate {
                 
                 self.score.text = String(self.points)
                 self.points = self.points + 1
-            
-            
-        }*/
+            */
         
-        let random = arc4random_uniform(0) + 220
-        let random1 = arc4random_uniform(0) + 224
-        let random2 = arc4random_uniform(0) + 239
-        let random3 = arc4random_uniform(0) + 280
-        let random4 = arc4random_uniform(0) + 330
-        let random5 = arc4random_uniform(0) + 330
-        let random6 = arc4random_uniform(00) + 400
-        let random7 = arc4random_uniform(0) + 400
-        let random8 = arc4random_uniform(0) + 400
-        let random9 = arc4random_uniform(0) + 450
-        let random10 = arc4random_uniform(0) + 550
-        let random11 = arc4random_uniform(0) + 650
-        let random12 = arc4random_uniform(0) + 650
-        let random13 = arc4random_uniform(0) + 650
-        let random14 = arc4random_uniform(0) + 650
-        let random15 = arc4random_uniform(0) + 650
-        let random16 = arc4random_uniform(0) + 680
-        let random17 = arc4random_uniform(0) + 710
-        let random18 = arc4random_uniform(0) + 710
-        let random19 = arc4random_uniform(0) + 710
-        let random20 = arc4random_uniform(0) + 740
-        let random21 = arc4random_uniform(0) + 740
-        let random22 = arc4random_uniform(0) + 740
-        let random23 = arc4random_uniform(0) + 900
-        let random24 = arc4random_uniform(0) + 980
-        let random25 = arc4random_uniform(0) + 980
-        let random26 = arc4random_uniform(0) + 980
-        let random27 = arc4random_uniform(0) + 980
-        let random28 = arc4random_uniform(0) + 980
-        let random29 = arc4random_uniform(0) + 980
-        let random30 = arc4random_uniform(0) + 980
-        let random31 = arc4random_uniform(0) + 980
-        let random32 = arc4random_uniform(0) + 980
-        let random33 = arc4random_uniform(0) + 980
-        let random34 = arc4random_uniform(0) + 1150
-        let random35 = arc4random_uniform(0) + 1150
-        let random36 = arc4random_uniform(0) + 1150
-        let random37 = arc4random_uniform(0) + 1150
-        let random38 = arc4random_uniform(0) + 1150
-        let random39 = arc4random_uniform(0) + 1150
-        let random40 = arc4random_uniform(0) + 1400
+        bird1.frame = CGRect(x: 900, y: 100, width: 100, height: 100)
+        bird2.frame = CGRect(x: 900, y: 200, width: 100, height: 100)
+        bird3.frame = CGRect(x: 1320, y: 73, width: 100, height: 100)
+        bird4.frame = CGRect(x: 1750, y: 20, width: 100, height: 100)
+        bird5.frame = CGRect(x: 2170, y: 231, width: 100, height: 100)
+        bird6.frame = CGRect(x: 2550, y: 152, width: 100, height: 100)
+        bird7.frame = CGRect(x: 3000, y: 293, width: 100, height: 100)
+        bird8.frame = CGRect(x: 3550, y: 24, width: 100, height: 100)
+        bird9.frame = CGRect(x: 4090, y: 95, width: 100, height: 100)
+        bird10.frame = CGRect(x: 4550, y: 250, width: 100, height: 100)
+        bird11.frame = CGRect(x: 5050, y: 10, width: 100, height: 100)
+        bird12.frame = CGRect(x: 5500, y: 190, width: 100, height: 100)
+        bird13.frame = CGRect(x: 6050, y: 73, width: 100, height: 100)
+        bird14.frame = CGRect(x: 6500, y: 204, width: 100, height: 100)
+        bird15.frame = CGRect(x: 6840, y: 31, width: 100, height: 100)
+        bird16.frame = CGRect(x: 7000, y: 152, width: 100, height: 100)
+        bird17.frame = CGRect(x: 7250, y: 263, width: 100, height: 100)
+
         
-        let randomx = arc4random_uniform(198) + 47
-        let randomx1 = arc4random_uniform(198) + 47
-        let randomx2 = arc4random_uniform(198) + 47
-        let randomx3 = arc4random_uniform(198) + 47
-        let randomx4 = arc4random_uniform(99) + 47
-        let randomx5 = randomx4 + 40
-        let randomx6 = arc4random_uniform(40) + 125
-        let randomx7 = randomx6 + 40
-        let randomx8 = randomx7 + 40
-        let randomx9 = arc4random_uniform(198) + 47
-        let randomx10 = arc4random_uniform(198) + 47
-        let randomx11 = arc4random_uniform(0) + 47
-        let randomx12 = randomx11 + 38
-        let randomx13 = randomx12 + 38
-        let randomx14 = randomx13 + 38
-        let randomx15 = randomx14 + 38
-        let randomx16 = arc4random_uniform(35) + 210
-        let randomx17 = arc4random_uniform(39) + 125
-        let randomx18 = randomx17 + 45
-        let randomx19 = randomx18 + 45
-        let randomx20 = arc4random_uniform(30) + 47
-        let randomx21 = arc4random_uniform(30) + 115
-        let randomx22 = arc4random_uniform(30) + 210
-        let randomx23 = arc4random_uniform(198) + 47
-        let randomx24 = arc4random_uniform(0) + 47
-        let randomx25 = arc4random_uniform(0) + 122
-        let randomx26 = arc4random_uniform(0) + 177
-        let randomx27 = arc4random_uniform(0) + 232
-        let randomx28 = arc4random_uniform(0) + 47
-        let randomx29 = randomx28 + 30
-        let randomx30 = randomx29 + 30
-        let randomx31 = randomx30 + 30
-        let randomx32 = randomx31 + 30
-        let randomx33 = randomx32 + 30
-        let randomx34 = arc4random_uniform(198) + 47
-        let randomx35 = arc4random_uniform(198) + 47
-        let randomx36 = arc4random_uniform(198) + 47
-        let randomx37 = arc4random_uniform(198) + 47
-        let randomx38 = arc4random_uniform(198) + 47
-        let randomx39 = arc4random_uniform(198) + 47
-        let randomx40 = arc4random_uniform(0) + 54
-        
-        bird1.frame = CGRect(x: -250, y:Int(randomx), width:100, height: 100)
-        bird2.frame = CGRect(x: -300, y:Int(randomx1), width:100, height: 100)
-        bird3.frame = CGRect(x: -350, y:Int(randomx2), width:100, height: 100)
-        bird4.frame = CGRect(x: -700, y:Int(randomx3), width:100, height: 100)
-        bird5.frame = CGRect(x: -1000, y:Int(randomx4), width:100, height: 100)
-        bird6.frame = CGRect(x: -1030, y:Int(randomx5), width:100, height: 100)
-        bird7.frame = CGRect(x: -1450, y:Int(randomx6), width:100, height: 100)
-        bird8.frame = CGRect(x: -1450, y:Int(randomx7), width:100, height: 100)
-        bird9.frame = CGRect(x: -1800, y:Int(randomx8), width:100, height: 100)
-        bird10.frame = CGRect(x: -2400, y: Int(randomx9), width:100, height: 100)
-        
-        coin.frame = CGRect(x: -250, y: Int(random3), width: 75, height: 75)
+        coin.frame = CGRect(x: 1600, y: 180, width: 70, height: 70)
+        coin1.frame = CGRect(x: 2200, y: 20, width: 70, height: 70)
+        coin2.frame = CGRect(x: 4200, y: 180, width: 70, height: 70)
+        coin3.frame = CGRect(x: 5200, y: 200, width: 70, height: 70)
+        coin4.frame = CGRect(x: 6100, y: 150, width: 70, height: 70)
+        coin5.frame = CGRect(x: 6100, y: 150, width: 70, height: 70)
         
         plane.myDelegate = self
         
         dynamicAnimator = UIDynamicAnimator(referenceView: self.view)
         
         
-        collisionBehavior = UICollisionBehavior (items: [bird1,bird2,bird3,bird4,bird5,bird6,bird7,bird8,bird9,bird10,coin])
+        collisionBehavior = UICollisionBehavior (items: [bird4,bird5,bird6,bird7,bird8,bird9,bird10,bird11,bird12,bird13,bird14,bird15,bird16,coin,coin1,coin2,coin3])
         
         collisionBehavior.translatesReferenceBoundsIntoBoundary = false
         dynamicAnimator.addBehavior(collisionBehavior)
-        dynamicBehavior = UIDynamicItemBehavior(items: [bird1,bird2,bird3,bird4,bird5,bird6,bird7,bird8,bird9,bird10,coin])
+        dynamicBehavior = UIDynamicItemBehavior(items: [bird4,bird5,bird6,bird7,bird8,bird9,bird10,bird11,bird12,bird13,bird14,bird15,bird16,coin,coin1, coin2,coin3])
         
        /* bird1.removeFromSuperview()
         bird2.removeFromSuperview()
@@ -262,23 +204,38 @@ class ViewController: UIViewController, subviewDelegate {
         bird5.removeFromSuperview()
         bird6.removeFromSuperview()
         */
-        //dynamicBehavior.elasticity = 0.8;
         
-        self.dynamicBehavior.addLinearVelocity(CGPoint(x: Int(random), y: 0), for: bird1)
-        self.dynamicBehavior.addLinearVelocity(CGPoint(x: Int(random1), y: 0), for: bird2)
-        self.dynamicBehavior.addLinearVelocity(CGPoint(x: Int(random2), y: 0), for: bird3)
-        self.dynamicBehavior.addLinearVelocity(CGPoint(x: Int(random3), y: 0), for: bird4)
-        self.dynamicBehavior.addLinearVelocity(CGPoint(x: Int(random4), y: 0), for: bird5)
-        self.dynamicBehavior.addLinearVelocity(CGPoint(x: Int(random5), y: 0), for: bird6)
-        self.dynamicBehavior.addLinearVelocity(CGPoint(x: Int(random6), y: 0), for: bird7)
-        self.dynamicBehavior.addLinearVelocity(CGPoint(x: Int(random7), y: 0), for: bird8)
-        self.dynamicBehavior.addLinearVelocity(CGPoint(x: Int(random8), y: 0), for: bird8)
-        self.dynamicBehavior.addLinearVelocity(CGPoint(x: Int(random9), y: 0), for: bird9)
         
-        self.dynamicBehavior.addLinearVelocity(CGPoint(x: Int(random3), y: 0), for: coin)
+        self.dynamicBehavior.addLinearVelocity(CGPoint(x: -350, y: 0), for: bird1)
+        self.dynamicBehavior.addLinearVelocity(CGPoint(x: -350, y: 0), for: bird2)
+        self.dynamicBehavior.addLinearVelocity(CGPoint(x: -350, y: 0), for: bird3)
+        self.dynamicBehavior.addLinearVelocity(CGPoint(x: -350, y: 0), for: bird4)
+        self.dynamicBehavior.addLinearVelocity(CGPoint(x: -450, y: 0), for: bird5)
+        self.dynamicBehavior.addLinearVelocity(CGPoint(x: -500, y: 0), for: bird6)
+        self.dynamicBehavior.addLinearVelocity(CGPoint(x: -550, y: 0), for: bird7)
+        self.dynamicBehavior.addLinearVelocity(CGPoint(x: -650, y: 0), for: bird8)
+        self.dynamicBehavior.addLinearVelocity(CGPoint(x: -650, y: 0), for: bird9)
+        self.dynamicBehavior.addLinearVelocity(CGPoint(x: -650, y: 0), for: bird10)
+        self.dynamicBehavior.addLinearVelocity(CGPoint(x: -650, y: 0), for: bird11)
+        self.dynamicBehavior.addLinearVelocity(CGPoint(x: -650, y: 0), for: bird12)
+        self.dynamicBehavior.addLinearVelocity(CGPoint(x: -750, y: 0), for: bird13)
+        self.dynamicBehavior.addLinearVelocity(CGPoint(x: -750, y: 0), for: bird14)
+        self.dynamicBehavior.addLinearVelocity(CGPoint(x: -850, y: 0), for: bird15)
+        self.dynamicBehavior.addLinearVelocity(CGPoint(x: -800, y: 0), for: bird16)
+        //self.dynamicBehavior.addLinearVelocity(CGPoint(x: -950, y: 0), for: bird17)
         
+        self.dynamicBehavior.addLinearVelocity(CGPoint(x: -350, y: 0), for: coin)
+        self.dynamicBehavior.addLinearVelocity(CGPoint(x: -420, y: 0), for: coin1)
+        self.dynamicBehavior.addLinearVelocity(CGPoint(x: -650, y: 0), for: coin2)
+        self.dynamicBehavior.addLinearVelocity(CGPoint(x: -650, y: 0), for: coin3)
+        self.dynamicBehavior.addLinearVelocity(CGPoint(x: -650, y: 0), for: coin4)
+        self.dynamicBehavior.addLinearVelocity(CGPoint(x: -750, y: 0), for: coin5)
+        //self.dynamicBehavior.addLinearVelocity(CGPoint(x: -350, y: 0), for: coin6)
+
         //coin.removeFromSuperview()
         dynamicAnimator.addBehavior(dynamicBehavior)
+        
+        dynamicBehavior.elasticity = 1;
     //self.view.sendSubview(toBack:roadimage)
         
         var treeArray : [UIImage]!
@@ -335,7 +292,16 @@ class ViewController: UIViewController, subviewDelegate {
         bird8.image = UIImage.animatedImage(with: birdArray, duration: 0.6)
         bird9.image = UIImage.animatedImage(with: birdArray, duration: 0.6)
         bird10.image = UIImage.animatedImage(with: birdArray, duration: 0.6)
-
+        bird11.image = UIImage.animatedImage(with: birdArray, duration: 0.6)
+        bird12.image = UIImage.animatedImage(with: birdArray, duration: 0.6)
+        bird13.image = UIImage.animatedImage(with: birdArray, duration: 0.6)
+        bird14.image = UIImage.animatedImage(with: birdArray, duration: 0.6)
+        bird15.image = UIImage.animatedImage(with: birdArray, duration: 0.6)
+        bird16.image = UIImage.animatedImage(with: birdArray, duration: 0.6)
+        bird17.image = UIImage.animatedImage(with: birdArray, duration: 0.6)
+        bird18.image = UIImage.animatedImage(with: birdArray, duration: 0.6)
+        bird19.image = UIImage.animatedImage(with: birdArray, duration: 0.6)
+        bird20.image = UIImage.animatedImage(with: birdArray, duration: 0.6)
     
         
         
